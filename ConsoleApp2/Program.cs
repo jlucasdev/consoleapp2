7e5bb8337738065805f54699ea7a7a0fee74808f
João Lucas <joaolucaspdsilva1@gmail.com>
2021-08-25 22:19:38 -0300

    3 commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            looping(1);
+        }
+
+        public static void looping(int a)
+        {
+            Console.WriteLine(a);
+            if(a < 10){
+                looping(a + 1);
+            }
+        }
+    }
+}
